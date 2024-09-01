d522c6a24fa499454408333790b9aa4637c0b006
= <mikeyk65@hotmail.com>
2024-09-01 20:20:33 +0100

    Changed from in memory data to SQLAlchemy SQLLite database

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from flask import Flask, request
-import flask
+#import flask
 
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, marshal_with, fields
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+SQLFILENAME = "sqlite:///database.db" 
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLFILENAME
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class DogModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable = False)
+    age = db.Column(db.Integer, nullable = False)
+    breed = db.Column(db.String(100), nullable = False)
+
+    # String representation of the object
+    def __repr__(self):
+        return f"Dog(name = {self.name}, age={self.age}, breed={self.breed})"
+
+# Creates database using the object
+# MUST ONLY RUN THIS ONCE SO WILL CHECK IF FILE EXISTS FIRST
+from pathlib import Path
+
+file = Path(SQLFILENAME)
+
+if file.exists():
+    pass
+else:
+    db.create_all()
 
 # Create a mini database to return details from 
 names = {
@@ -25,33 +51,96 @@ dogPutArgs.add_argument("name", type=str, help="Name of the dog is required", re
 dogPutArgs.add_argument("age", type=int, help = "Age of the dog is required", required = True)
 dogPutArgs.add_argument("breed", type=str, help = "What breed of dog is required", required = True)
 
-# In memory dogs database
-dogs = {}
+dogUpdateArgs = reqparse.RequestParser()
+dogUpdateArgs.add_argument("name", type=str, help="Name of the dog is required")
+dogUpdateArgs.add_argument("age", type=int, help = "Age of the dog is required")
+dogUpdateArgs.add_argument("breed", type=str, help = "What breed of dog is required")
 
-def abortIfDogExists (dogId):
+""" def abortIfDogExists (dogId):
     if dogId in dogs:
         abort(409, message="Dog already exists with Id " + str(dogId))
 
 def abortIfDogDoesNotExist(dogId):
     if dogId not in dogs:
         abort(404, message="Dog Id " + str(dogId) + " is not valid")
-        
+ """        
+
+resourceFields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "age": fields.Integer,
+    "breed": fields.String
+}
 class Dog(Resource):
+    @marshal_with(resourceFields)
     def get(self, dogId):
-        abortIfDogDoesNotExist(dogId)
-        return dogs[dogId]
+        result = DogModel.query.filter_by(id = dogId).first()
+
+        if not result:
+            abort(404, message="Could not find Dog")
+
+        return result
     
     def put (self, dogId):
-        abortIfDogExists(dogId)
         args = dogPutArgs.parse_args()
-        dogs[dogId] = args
+        result = DogModel.query.filter_by(id = dogId).first()
+        if result:
+            abort(409, message="Dog ID already taken")
+                  
+        dog = DogModel (id = dogId, name = args["name"], age = args["age"], breed = args["breed"])
+
+        try:
+            db.session.add (dog)
+            db.session.commit()
         
-        return dogs[dogId], 201
+        except Exception as e:
+            db.session.rollback()
+            print(f"An error occurred: {e}")
+
+        return dog, 201
     
+    def patch(self, dogId):
+        args = dogUpdateArgs.parse_args()
+        result = DogModel.query.filter_by(id = dogId).first()
+        if not result:
+            abort(409, message="Dog not found - cannot update")
+
+        if args["name"]:
+            result.name = args["name"]
+        if args["age"]:
+            result.age = args["age"]
+        if args["breed"]:
+            result.breed = args["breed"]
+        
+        try:
+            #db.session.add(result)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print(f"An error occurred: {e}")
+
+        return result, 201
+
+    # TO DO
+    def update (self, dogId):
+        args = dogPutArgs.parse_args()
+        
+        return
+    
+    # TO DO
     def delete (self, dogId):
-        abortIfDogDoesNotExist (dogId)
-        del dogs[dogId]
-        return '', 204
+        result = DogModel.query.filter_by(id = dogId).first()
+        if not result:
+            abort(409, message="Dog not found - cannot delete")
+
+        try:
+            db.session.delete(result)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print(f"An error occurred trying to delete dog {dogId}: {e}")         
+        
+        return 201
 
 # Add endpoint
 api.add_resource (HelloWorld, "/helloworld/<string:name>")
--- test.py
+++ test.py
@@ -2,30 +2,41 @@ import requests
 
 BASEURL = "http://127.0.0.1:5000/"
 
-response = requests.get(BASEURL + "helloworld/mike")
-print (response.json())
+#response = requests.get(BASEURL + "helloworld/mike")
+#print (response.json())
 
 #response = requests.post(BASEURL + "helloworld")
 #print (response.json())
 
+#response = requests.patch(BASEURL + "dogs/1", {"name":"Tazzy", "age":3})
+#print (response)
+
+
 
 response = requests.put(BASEURL + "dogs/1", {"name":"Tazzy", "age":5, "breed":"bulldog"})
-response = requests.put(BASEURL + "dogs/2", {"name":"Tazzy2", "age":6, "breed":"spaniel"})
-response = requests.put(BASEURL + "dogs/3", {"name":"Tazzy3", "age":7, "breed":"terrier"})
-response = requests.put(BASEURL + "dogs/4", {"name":"Tazzy4", "age":8, "breed":"Collie"})
+#response = requests.put(BASEURL + "dogs/2", {"name":"Tazzy2", "age":6, "breed":"spaniel"})
+#response = requests.put(BASEURL + "dogs/3", {"name":"Tazzy3", "age":7, "breed":"terrier"})
+#response = requests.put(BASEURL + "dogs/4", {"name":"Tazzy4", "age":8, "breed":"Collie"})
+#response = requests.put(BASEURL + "dogs/5", {"name":"Tazzy5", "age":1, "breed":"husky"})
 #print (response.json())
-input()
+
+
+#response = requests.delete(BASEURL + "dogs/1")
+#print (response)
+
+
+
 response = requests.get(BASEURL + "dogs/1")
 print (response.json())
-response = requests.get(BASEURL + "dogs/2")
-print (response.json())
-response = requests.get(BASEURL + "dogs/3")
-print (response.json())
-response = requests.get(BASEURL + "dogs/6")
-print (response.json())
-response = requests.delete(BASEURL + "dogs/1")
-print (response)
-response = requests.get (BASEURL + "dogs/1")
-print (response.json())
+#response = requests.get(BASEURL + "dogs/2")
+#print (response.json())
+#response = requests.get(BASEURL + "dogs/3")
+#print (response.json())
+#response = requests.get(BASEURL + "dogs/6")
+#print (response.json())
+#response = requests.delete(BASEURL + "dogs/1")
+#print (response)
+#response = requests.get (BASEURL + "dogs/1")
+#print (response.json())
 
 
